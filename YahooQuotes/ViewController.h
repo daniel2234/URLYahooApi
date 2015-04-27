1e186ec6e1de7a5e7b35b5bcae0532afcdda4e37
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2015-04-27 18:51:24 -0400

    added quotes and charts for user input

--- YahooQuotes/ViewController.h
+++ YahooQuotes/ViewController.h
@@ -12,11 +12,17 @@
 
 //UITextField
 @property (strong, nonatomic) UITextField *textField;
+@property (strong,nonatomic) UITextField  *timeChartField;
 //String
 @property (strong,nonatomic) NSString *quotes;
 //UILabel
-@property (strong,nonatomic) UILabel *myLabel;
+@property (strong,nonatomic) UILabel *priceLabel;
+@property (strong,nonatomic) UILabel *dateLabel;
+@property (strong,nonatomic) UILabel *timeLabel;
+@property (strong,nonatomic) UILabel *priceChangeLabel;
 
+//UIImageView
+@property(strong,nonatomic)UIImageView *chartImageView;
 
 @end
 
--- YahooQuotes/ViewController.m
+++ YahooQuotes/ViewController.m
@@ -8,13 +8,32 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    //create the testfield
+    //create the textfield
     self.textField = [[UITextField alloc]initWithFrame:CGRectMake(20, 60, 280, 20)];
-    self.textField.text = @"AAPL";
+    self.textField.text = @"";
+    self.textField.placeholder = @"Input Ticker Symbol";
     [self.view addSubview:_textField];
-    self.myLabel = [[UILabel alloc]initWithFrame:CGRectMake(20, 100, 280, 40)];
-    self.myLabel.text = @"0.0";
-    [self.view addSubview:_myLabel];
+    //create textfield for monthly charts
+    self.timeChartField = [[UITextField alloc]initWithFrame:CGRectMake(20, 100, 280, 20)];
+    self.timeChartField.text = @"";
+    self.timeChartField.placeholder = @"Input Months ";
+    [self.view addSubview:_timeChartField];
+
+    self.priceLabel = [[UILabel alloc]initWithFrame:CGRectMake(20, 120, 280, 40)];
+    self.priceLabel.text = @"$0000000.00";
+    [self.view addSubview:_priceLabel];
+    
+    self.dateLabel = [[UILabel alloc]initWithFrame:CGRectMake(20, 140, 280, 40)];
+    self.dateLabel.text = @"00/00/0000";
+    [self.view addSubview:_dateLabel];
+    
+    self.timeLabel = [[UILabel alloc]initWithFrame:CGRectMake(20, 160, 280, 40)];
+    self.timeLabel.text = @"00:00";
+    [self.view addSubview:_timeLabel];
+    
+    self.priceChangeLabel = [[UILabel alloc]initWithFrame:CGRectMake(20, 180, 280, 40)];
+    self.priceChangeLabel.text = @"000.00";
+    [self.view addSubview:_priceChangeLabel];
     
     UIButton *myButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     myButton.frame = CGRectMake(20, 200, 280, 40);
@@ -22,9 +41,26 @@
     [myButton addTarget:self action:@selector(getQuote) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:myButton];
     
+    self.chartImageView = [[UIImageView alloc] init];
+    self.chartImageView.frame = CGRectMake(60.0, 400.0, 300.0, 300.0);
+    [self.view addSubview:self.chartImageView];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+}
+//show data from chart
+-(void)getChart
+{
+    NSString *chartURL = [NSString stringWithFormat:@"http://chart.finance.yahoo.com/z?s=%@&t=%@m",_textField.text,_timeChartField.text];
+    UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:chartURL]]];
+    self.chartImageView.image = image;
 }
 
 -(void)getQuote {
+    [self getChart];
     //http://download.finance.yahoo.com/d/quotes.csv?s=%@&f=sl1d1t1c1ohgv&e=.csv
     NSString *quoteAddress = [NSString stringWithFormat:@"http://download.finance.yahoo.com/d/quotes.csv?s=%@&f=sl1d1t1c1ohgv&e=.csv",_textField.text];
     NSURL *theURL = [[NSURL alloc]initWithString:[quoteAddress stringByAddingPercentEscapesUsingEncoding:NSASCIIStringEncoding]];//create th url with string
@@ -34,7 +70,11 @@
     NSData *data = [NSURLConnection sendSynchronousRequest:theRequest returningResponse:&response error:&error];//createing a data object 
     NSMutableString *contentString = [[NSMutableString alloc]initWithData:data encoding:NSUTF8StringEncoding];
     NSArray *arrayData = [contentString componentsSeparatedByString:@","];
-    _myLabel.text = [arrayData objectAtIndex:1];
+    _priceLabel.text = [arrayData objectAtIndex:1];
+    _dateLabel.text = [arrayData objectAtIndex:2];
+    _timeLabel.text = [arrayData objectAtIndex:3];
+    _priceChangeLabel.text = [arrayData objectAtIndex:4];
+    
     NSLog(@"%@",contentString);
 
 }
